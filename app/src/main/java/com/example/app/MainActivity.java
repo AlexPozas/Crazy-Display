ececc905cda3f790e9ca108f82ab7ecb112b9c93
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2023-11-09 18:25:03 +0100

    9

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +21,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button ran = findViewById(R.id.mesages);
-        ran.setOnClickListener(new View.OnClickListener(){
+        final Button ran = findViewById(R.id.mesages);//mensages
+        EditText campoTexto = (EditText) findViewById(R.id.missatge);
+        EditText ip = (EditText) findViewById(R.id.ip);
+        try {
+            URI uri = new URI("ws://localhost:8080/websocket");
+            WebSocketClient webSocketClient = new WebSocketClient(uri) {
+                @Override
+                public void onOpen(ServerHandshake handshakedata) {
+                    // Se ha conectado al servidor
+                }
 
+                @Override
+                public void onMessage(String message) {
+                    // Se ha recibido un mensaje del servidor
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    // Se ha desconectado del servidor
+                }
+
+                @Override
+                public void onError(Exception ex) {
+                    // Se ha producido un error
+                }
+            };
+
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        final Button conn = findViewById(R.id.conectar);//conectar
+        conn.setOnClickListener(new View.OnClickListener(){
+             @Override
+            public void onClick(View view) {
+
+
+                }
+                });
+
+        ran.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
+                if (LlistaActivity.mis.contains(campoTexto.getText().toString())){
+                    Log.i("INFO",campoTexto.getText().toString());
+                }
+                else {LlistaActivity.mis.add(campoTexto.getText().toString());
+
+                }
+
                 Intent intent = new Intent(MainActivity.this,LlistaActivity.class);
                 startActivity(intent);
 
